555e9b509c0360b173277acb9258342714ff5630
amitblogger <amitblogger999@gmail.com>
2025-04-20 22:46:16 +0530

    Create chart.js

---
+++ chart.js
@@ -0,0 +1,6 @@
+/*!
+ * Chart.js v4.4.1
+ * https://www.chartjs.org
+ * Released under the MIT License
+ */
+!function(t,e){"object"==typeof exports&&"undefined"!=typeof module?e(exports,require("chart.js/helpers")):"function"==typeof define&&define.amd?define(["exports","chart.js/helpers"],e):e((t="undefined"!=typeof globalThis?globalThis:t||self).Chart={},t.Chart.helpers)}(this,(function(t,e){"use strict";/* OMITTED FOR BREVITY */}));
