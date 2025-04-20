81b41a7a25ed269c7058bd0d0c7fa6bee21a4884
amitblogger <amitblogger999@gmail.com>
2025-04-20 22:42:52 +0530

    Create smart-bmi.php

---
+++ smart-bmi.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Smart BMI & Health Tracker
+ * Plugin URI: https://yourwebsite.com/
+ * Description: A modern BMI calculator plugin with gender toggle, chart, health tips, and ideal weight range.
+ * Version: 1.0.0
+ * Author: AD Media Network
+ * Author URI: https://yourwebsite.com/
+ * License: GPL2
+ */
